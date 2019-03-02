7998dc2ee3b5145c0ca08612b5b43d68167a129a
Michelle Beshears <meeshysu@gmail.com>
2019-03-02 09:51:25 -0600

    added this stuff yeah

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type in something");
+            string s = Console.ReadLine().ToLower();
+            string output = "";
+                for (int i = 0; i < s.Length; i++)
+            {
+                output += char.ToUpper(s[i]);
+
+                for (int repeat = 0; repeat < i; repeat++)
+                {
+                    output += (s[i]);
+                }
+                output += "-";
+            }
+            string remove = output.Remove(output.Length - 1);
+            Console.WriteLine(remove);
+            Console.ReadLine();
         }
     }
 }
